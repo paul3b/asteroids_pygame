97079a3e61228a5baae3620707f1c5545ccd1ac9
paul3b <pauleblain@outlook.com>
2025-02-05 22:10:18 -0700

    creating more stuff

--- main.py
+++ main.py
@@ -1,8 +1,19 @@
-import pygame
-
+import pygame # type: ignore
+from constants import *
 
 def main():
     print("Starting asteroids!")
+    
+    #initialize pygame
+    pygame.init()
+    screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+    
+    #Create the game loop
+    while True:
+        screen.fill((0,0,0))
+        pygame.display.flip()
+
+
 
 if __name__ == "__main__":
     main()
