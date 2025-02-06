3e7ec4938c35258a82947d01a464765ab345d185
paul3b <pauleblain@outlook.com>
2025-02-06 12:18:29 -0700

    added sprites + gui + movement

--- main.py
+++ main.py
@@ -1,5 +1,8 @@
 import pygame # type: ignore
 from constants import *
+from player import Player
+from asteroid import Asteroid
+from asteroidfield import AsteroidField
 
 def main():
     print("Starting asteroids!")
@@ -7,12 +10,40 @@ def main():
     #initialize pygame
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
-    
+    clock = pygame.time.Clock()
+
+    updatable = pygame.sprite.Group()
+    drawable = pygame.sprite.Group()
+    asteroids = pygame.sprite.Group()
+
+    Asteroid.containers = (asteroids, updatable, drawable)
+    AsteroidField.containers = updatable
+    asteroid_field = AsteroidField()
+
+    Player.containers = (updatable,drawable)
+
+    player = Player(SCREEN_WIDTH/2,SCREEN_HEIGHT/2)
+
+    dt = 0
+
     #Create the game loop
     while True:
-        screen.fill((0,0,0))
+        #check if the user has closed the window and exit the game loop if they do.
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                return
+        
+        updatable.update(dt)
+
+        screen.fill("black")
+        
+        for obj in drawable:
+            obj.draw(screen)
+        
         pygame.display.flip()
 
+        #limit framerate
+        dt = clock.tick(60) / 1000
 
 
 if __name__ == "__main__":
